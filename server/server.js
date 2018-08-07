f84d678574d48304b469f0f7719d8dd49b657a24
ertus0409 <ertus0409@hotmail.com>
2018-08-07 15:15:49 +0300

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-    res.status(400).send('Unable to save todo: ', e)
+    res.status(400).send('Unable to save todo: ')
   });
 });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}, {
+  text: 'Third test todo'
+}]
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -25,8 +35,8 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(1);
-          expect(todos[0].text).toBe(text);
+          expect(todos.length).toBe(4);
+          expect(todos[3].text).toBe(text);
           done();
         }).catch((e) => done(e));
       });
@@ -37,15 +47,27 @@ describe('POST /todos', () => {
     request(app)
       .post('/todos')
       .send({})
-      .expect(200)
+      .expect(400)
       .end((err, res) => {
         if (err) {
           return done(err);
         }
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(3);
         done();
       }).catch((e) => done(e));
       });
   });
 });
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(3);
+      })
+      .end(done);
+  })
+})
