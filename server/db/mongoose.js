46b16f18a7b21d3225442e2ec5563cd96ca1db21
ertus0409 <ertus0409@hotmail.com>
2018-08-09 15:31:03 +0300

    Mongoose connect bug fix 01

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(('mongodb://ertugrulucar:356145ad@ds113692.mlab.com:13692/todo-api' || 'mongodb://127.0.0.1:27017/TodoApp'), { useNewUrlParser: true });
+mongoose.connect((process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp'), { useNewUrlParser: true });
 
 module.exports = {mongoose};
